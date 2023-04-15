1104c16b110afb26c6f4f2fa87fe2ae46d71cc52
minhhung123 <prohungnguyenminh@gmail.com>
2023-04-15 14:56:29 +1000

    Added process directory function

--- ingest/ingest.ts
+++ ingest/ingest.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import { Document } from "langchain/document";
 import { load } from "cheerio";
+import path from "path";
 
 const processFile = async (filePath: string): Promise<Document> => {
     return await new Promise<Document>((resolve, reject) => {
@@ -15,4 +16,31 @@ const processFile = async (filePath: string): Promise<Document> => {
             }
         })
     })
+}
+
+const processDirectory = async (directoryPath: string): Promise<Document[]> => {
+    const docs: Document[] = [];
+    let files: string[];
+    try {
+        files = fs.readdirSync(directoryPath);
+    } catch (err) {
+        console.error(err);
+        throw new Error(
+            `Could not read directory: ${directoryPath}.`
+        );
+    }
+    for (const file of files) {
+        const filePath = path.join(directoryPath, file);
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            const newDocs = processDirectory(filePath);
+            const nestedDocs = await newDocs;
+            docs.push(...nestedDocs);
+        } else {
+            const newDoc = processFile(filePath);
+            const doc = await newDoc;
+            docs.push(doc);
+        }
+    }
+    return docs;
 }
